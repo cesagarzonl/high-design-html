9aab6cf59c91390e4c12aca9ce7bf1ab341d6457
Csar Garzon Lamprea <cigarzon8@misena.edu.co>
2016-06-17 10:06:41 -0500

    Add files via upload

---
+++ tegiendo.js
@@ -0,0 +1,9 @@
+function busca() {
+    var cx = '012584391112063194925:q_5tmq3zvz0';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  }
